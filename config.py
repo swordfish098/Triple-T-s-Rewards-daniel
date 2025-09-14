717089e6ee39169d3001179d907ea1126f4e3ae7
dagriff0 <dagriff@clemson.edu>
2025-09-14 18:31:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fdf3b077e67f77d9b75322212cd0d50f8932ed96db890458caaeab3aa27c2f44')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'mysql+pymysql://root:astrok120@localhost/tripletdb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    #This comment is a test for Azure git commits
